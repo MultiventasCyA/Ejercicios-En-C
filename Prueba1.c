4a1909bb1483f90f6460988c514d1db8f05c02f5
Alessandro <112961384+MultiventasCyA@users.noreply.github.com>
2026-02-27 15:41:04 -0600

    Add:Comentarios en las lineas de los numero

--- Prueba1.c
+++ Prueba1.c
@@ -4,15 +4,15 @@ int main()
 {
   float numero1, numero2, numero3;
 
-  printf("Promedio de tres numeros\n");
-  printf("Ingrese el primer numero: ");
+  printf("Promedio de tres numeros\n"); //tres numero
+  printf("Ingrese el primer numero: "); //numero 1
   scanf("%f", &numero1);
-  printf("Ingrese el segundo numero: ");
+  printf("Ingrese el segundo numero: "); //numero 2
   scanf("%f", &numero2);
-  printf("Ingrese el tercer numero: ");
+  printf("Ingrese el tercer numero: "); //numero3
   scanf("%f", &numero3);
 
-  printf("El promedio de los tres numeros es: %.2f", (numero1 + numero2 + numero3) / 3.0);
+  printf("El promedio de los tres numeros es: %.2gf", (numero1 + numero2 + numero3) / 3.0);
 
   return 0; //no retorna nada
 }
