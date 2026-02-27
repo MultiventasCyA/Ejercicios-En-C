e46873eac4ed3a8d2ccba1b3b27b6c84636272ed
Alessandro <112961384+MultiventasCyA@users.noreply.github.com>
2026-02-27 16:15:35 -0600

    Ajustar los comentarios

--- Taller Mecanico.c
+++ Taller Mecanico.c
@@ -65,23 +65,23 @@ int main()
 	precioFinal = COSTO_FIJO + (COSTO_FIJO * porcentajeExtra / 100.0); // operacion para asignarle al precioFinal, costo fijo + el % extra.
 
 	// Calcular fecha de entrega (3 dias despues)
-	struct tm fechaIngreso = {0}; /*es una estructura que descompone una fecha en componentes: año, mes, dia, hora, minutos, segundos.
-	El 0 es para limpiar desde el inicio todos los campos de la estructura.*/
+
+	//! es una estructura que descompone una fecha en componentes: año, mes, dia, hora, minutos, segundos. El 0 es para limpiar desde el inicio todos los campos de la estructura
+	struct tm fechaIngreso = {0};
 	fechaIngreso.tm_mday = dia;
-	fechaIngreso.tm_mon = mes - 1;			// (0 = enero, 11 = diciembre)
-	fechaIngreso.tm_year = anio - 1900; // años desde 1900 como año 0, 2026 es 126 (2026-1900=126)
+	fechaIngreso.tm_mon = mes - 1;			//! (0 = enero, 11 = diciembre)
+	fechaIngreso.tm_year = anio - 1900; //! años desde 1900 como año 0, 2026 es 126 (2026-1900=126)
 	fechaIngreso.tm_hour = hora;
 	fechaIngreso.tm_min = minuto;
 
-	/* "time_t tiempo" Es una variable que guarda un instante de tiempo en segundos desde el 1 de enero de 1970 (Epoch Unix).*/
-	/* La funci�n "mktime" revisa la estructura. Al detectar que el d�a es 33, esta misma funcion recalcula la fecha: ajusta el mes,
-	el d�a sobrante y, si es necesario, el a�o.*/
+	//! "time_t tiempo" Es una variable que guarda un instante de tiempo en segundos desde el 1 de enero de 1970 (Epoch Unix).*/
+	//! La funcion "mktime" revisa la estructura. Al detectar que el dia es 33, esta misma funcion recalcula la fecha: ajusta el mes,	el dia sobrante y, si es necesario, el año.
 	time_t tiempo = mktime(&fechaIngreso);
-	tiempo += DIAS_ENTREGA * 24 * 3600; // sumar 3 d�as en segundos (3 dias = 259,200 segundos).
+	tiempo += DIAS_ENTREGA * 24 * 3600; //! sumar 3 dias en segundos (3 dias = 259,200 segundos).
 
 	struct tm fechaEntrega = *localtime(&tiempo);
 
-	// Comprobante para el  cliente
+	//! Comprobante para el  cliente
 	printf("---------------------------------------------\n");
 	printf("---COMPROBANTE DEL CLIENTE---\n");
 	printf("Taller: Hermanos Benavidez \n");
