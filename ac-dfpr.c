407f34317ab7d8fb8336d3de48244bdc5a8c9770
Alessandro <112961384+MultiventasCyA@users.noreply.github.com>
2026-02-28 23:31:58 -0600

    ADD: agrego el menu

--- ac-dfpr.c
+++ ac-dfpr.c
@@ -9,8 +9,27 @@ struct producto
    int vendidos;
 };
 
+void menu();
+
 int main()
 {
+   int opcion;
+
+   menu();
+   scanf("%d", &opcion);
 
    return 0;
+}
+
+void menu()
+{
+   printf("-----------------------------------------------\n");
+   printf("-----------------MENU PRINCIPAL----------------\n");
+   printf("-----------------------------------------------\n");
+   printf("1) Capturar inventario inicial\n");
+   printf("2) Registrar una venta\n");
+   printf("3) Mostrar reporte del dia\n");
+   printf("4) Salir\n");
+   printf("-----------------------------------------------\n");
+   printf("Elige una opcion: ");
 }
